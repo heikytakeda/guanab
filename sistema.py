b2a031297f55564ee74b6934b2fb88cc7e6e8daa
heikytakeda <heikytakeda010@gmail.com>
2026-02-14 03:28:45 -0300

    ajustes pt2

--- sistema.py
+++ sistema.py
@@ -1,6 +1,18 @@
 from .test import cab
 from .test import menu
 from .test import linha
+from time import sleep
 
 cab('SISTEMA DE ARQUIVOS')
-resposta = menu(['Criar arquivos', 'Cadastrar pessoas', 'Lista de pessoas'])
+while True:
+  resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+  if resposta == 1:
+    cab('opc 1')
+  elif resposta == 2:
+    cab('opc 2')
+  elif resposta == 3:
+    cab('Saindo do sistema...ate logo!')
+    break
+  else:
+    print('\033[31mERRO! Digite uma opcao valida.\033[m')
+  sleep(1)
